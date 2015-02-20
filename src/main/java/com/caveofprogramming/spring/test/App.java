be2a0e8b94e626b005e93a2e67270b7bdf6b3363
huntleja <huntleyjohn1992@gmail.com>
2015-02-19 23:33:04 -0500

    Demonstrating autowiring by type, while skirting ambiguities.

--- src/main/java/com/caveofprogramming/spring/test/App.java
+++ src/main/java/com/caveofprogramming/spring/test/App.java
@@ -3,25 +3,18 @@ package com.caveofprogramming.spring.test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Created by John on 2/17/2015.
- */
 public class App {
+
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
 
+        Logger logger = (Logger) context.getBean("logger");
 
-        Person person = (Person) context.getBean("person");
-
-        person.setTaxId(666);
-
-        Address address2 = (Address) context.getBean("address2");
-        System.out.println(address2.toString());
-
-        //System.out.println(person);
+        logger.writeConsole("Hello there");
+        logger.writeFile("Hi again");
 
         ((ClassPathXmlApplicationContext) context).close();
-
     }
+
 }
---
+++ src/main/java/com/caveofprogramming/spring/test/ConsoleWriter.java
@@ -0,0 +1,8 @@
+package com.caveofprogramming.spring.test;
+
+public class ConsoleWriter implements LogWriter {
+
+    public void write(String text) {
+        System.out.println(text);
+    }
+}
