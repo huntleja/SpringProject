d20efabcc28372fd98b89f4fc8769c5a3a0fd3c0
huntleja <huntleyjohn1992@gmail.com>
2015-02-20 00:17:25 -0500

    Autowiring with annotations.  Demonstrating setting required to false.

--- src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -4,15 +4,26 @@ package com.caveofprogramming.spring.test;
  * Dummy implementation of logger.
  */
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Logger {
+
 
     private ConsoleWriter consoleWriter;
     private FileWriter fileWriter;
 
+    /*@Autowired
+    public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter) {
+        this.consoleWriter = consoleWriter;
+        this.fileWriter = fileWriter;
+    }*/
+
+    @Autowired(required = false)
     public void setConsoleWriter(ConsoleWriter writer) {
         this.consoleWriter = writer;
     }
 
+    @Autowired
     public void setFileWriter(FileWriter fileWriter) {
         this.fileWriter = fileWriter;
     }
@@ -22,6 +33,7 @@ public class Logger {
     }
 
     public void writeConsole(String text) {
+        if (consoleWriter != null)
         consoleWriter.write(text);
     }
 
